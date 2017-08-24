4106e6fdca246e56b29a72ff428a089cb486025d
Anelya <aneli@notan.ua>
2017-08-25 00:31:58 +0300

    Adding model comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
 
 class PostAdmin(admin.ModelAdmin):
 	fields = ['author','image', 'title', 'text', 'created_date', 'published_date']
 
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Comment)
 
 		
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('blog.urls')),
+	url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
 
 if settings.DEBUG:
